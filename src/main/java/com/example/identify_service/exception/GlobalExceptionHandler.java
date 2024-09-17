8f20c5ab4a0a1730f3bf5491f76ce7836b50f53d
Phuc <phucnvt.22itb@vku.udn.vn>
2024-09-17 15:48:32 +0700

    handling exception

--- src/main/java/com/example/identify_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/identify_service/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice //Khai báo để biết đây là nơi quản lý các exception
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
